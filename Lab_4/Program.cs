08993e703989075a8a14e0f17d53cd9f6b507ce3
chrom <chrom@DESKTOP-NIKITA>
2021-11-23 15:34:08 +0300

    Добавьте файлы проекта.

---
+++ Lab_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N>0");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            if (N > 0)
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    sum += (2 * i) - 1;
+                    Console.WriteLine("Квадрат числа {0} равен {1}", i, sum);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Число N=0!");
+            }
+         
+            Console.ReadKey();
+        }
+    }
+}
